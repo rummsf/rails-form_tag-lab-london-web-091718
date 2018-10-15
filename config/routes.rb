5071d7ffebf500121cc7032b890a1c7445983e84
Irum Fazal <irummf@gmail.com>
2018-10-15 14:25:55 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create] 
   get '/students/:id', to: 'students#show', as: 'student'
+  
 end
+
+# describe 'form page' do
+#   it 'renders the form with the new action' do
+#     visit new_student_path
+#     expect(page).to have_content("Student Form")
+#   end
+
+#   it 'ensures that the new form submits content and renders form content' do
+#     visit new_student_path
+
+#     fill_in 'first_name', with: "Margaery"
+#     fill_in 'last_name', with: "Tyrell"
+
+#     click_on "Submit Student"
+
+#     expect(page).to have_content("Margaery")
+#   end
+# end
